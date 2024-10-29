fc708cb705cd05fa98ce3f118022f841059c6b78
oversize <russciyvasia@bk.ru>
2024-10-29 12:43:08 +0700

    Level5

--- project/index.php
+++ project/index.php
@@ -1,135 +1,34 @@
 <?php
-/*$array = [
-    'level1' => [
-        'level2' => [
-            'level3' => [
-                'element1' => 'value1',
-                'element2' => 'value2'
-            ]
-        ]
-    ]
-];
-
-$array['level1']['level2']['level3']['element3'] = 'value3';
-$array['level1']['level2']['level3']['element4'] = 'value4';
-
-echo print_r($array);
-*/
-
-/*
-$carsSpeeds = [
-    95,
-    140,
-    78
-];
-
-$sumOfSpeeds = 0;
-$countOfCars = 0;
-
-foreach ($carsSpeeds as $speed) {
-    $sumOfSpeeds += $speed;
-    $countOfCars++;
+function printOwnCode() {
+    $code = file_get_contents(__FILE__);
+    echo htmlspecialchars($code);
 }
 
-$averageSpeed = $sumOfSpeeds / $countOfCars;
-
-echo 'Средняя скорость движения по трассе: ' . $averageSpeed;
-*/
-/*
-$evenNumbers = [];
-$number = 345;
-
-while ($number <= 357) {
-    if ($number % 2 == 0) {
-        $evenNumbers[] = $number;
-    }
-    $number++;
-}
-
-foreach ($evenNumbers as $number) {
-    echo $number . "<br>";
-}
-*/
-/*
-$input = 'hello world';
-$words = explode(' ', $input);
-
-$reversedWords = [];
-for ($i = count($words) - 1; $i >= 0; $i--) {
-    $reversedWords[] = $words[$i];
-}
-
-$output = implode(' ', $reversedWords);
-echo $output;
-*/
-/*
-$input = 'hello world';
-$output = strtoupper($input);
-echo $output;
-*/
-
-/*
-function containsValue($array, $value)
-{
-    foreach ($array as $item) {
-        if ($item === $value) {
-            return true;
+function listAllFiles() {
+    $files = scandir(__DIR__);
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..') {
+            echo $file . PHP_EOL;
         }
     }
-    return false;
 }
-$array = [1, 2, 3, 4, 5];
-$value = 3;
-echo containsValue($array, $value) ? 'true<br>' : 'false<br>';
 
-function countOccurrences($array, $value)
-{
-    $count = 0;
-    foreach ($array as $item) {
-        if ($item === $value) {
-            $count++;
+function listDirectories() {
+    $files = scandir(__DIR__);
+    foreach ($files as $file) {
+        if ($file !== '.' && $file !== '..' && is_dir($file)) {
+            echo $file . PHP_EOL;
         }
     }
-    return $count;
 }
-$array = [1, 2, 1, 3];
-$value = 1;
-echo countOccurrences($array, $value) . '<br>';
 
-function fibonacci($n)
-{
-    $sequence = [];
-    if ($n >= 1) {
-        $sequence[] = 0;
-    }
-    if ($n >= 2) {
-        $sequence[] = 1;
-    }
-    for ($i = 2; $i < $n; $i++) {
-        $sequence[] = $sequence[$i - 1] + $sequence[$i - 2];
-    }
-    return implode(' ', $sequence);
-}
-$n = 10;
-echo fibonacci($n);
-*/
-/*
-$array = [1, 3, 2];
-sort($array);
-$result = implode(':', $array);
-echo $result . '<br>';
+echo "Собственный код программы:" . PHP_EOL . '<br>';
+printOwnCode();
+echo PHP_EOL;
 
+echo '<br>' ."Список всех файлов в текущей директории:" . PHP_EOL . '<br>';
+listAllFiles();
+echo PHP_EOL;
 
-$array = [1, 2, 3, 4, 5];
-$subArray = array_slice($array, 1, 3);
-echo print_r($subArray) . '<br>';
-
-$input = '0 2 3 1 2';
-$numbers = explode(' ', $input);
-$uniqueNumbers = array_unique($numbers);
-$output = implode(' ', $uniqueNumbers);
-echo $output;
-*/
-
-
-?>
+echo '<br>' . "Список только папок в текущей директории:" . PHP_EOL . '<br>';
+listDirectories();
---
+++ project/tasks.php
@@ -0,0 +1,20 @@
+<?php
+
+function generatePairs($input)
+{
+    $numbers = array_map('intval', explode(' ', $input));
+    $pairs = [];
+    foreach ($numbers as $i) {
+        foreach ($numbers as $j) {
+            if ($i != $j) {
+                $pairs[] = [$i, $j];
+            }
+        }
+    }
+    foreach ($pairs as $pair) {
+        echo $pair[0] . ' ' . $pair[1] . PHP_EOL;
+    }
+}
+
+$input = "1 2 3";
+generatePairs($input);
